cb6d99c44fa7d7de777bc668229e64761ec7b8b1
Kai Peintinger <kai.peintinger@outlook.com>
2023-11-18 12:25:22 +0100

    Initial Commit

---
+++ PrismControl/PrismControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrismControlApp.swift
+//  PrismControl
+//
+//  Created by Kai Peintinger on 18.11.23.
+//
+
+import SwiftUI
+
+@main
+struct PrismControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
